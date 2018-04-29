2e0c16eef11f96e33c5ef3bf50d58260392782f3
MihaiPopescu1985 <justmaryo@gmail.com>
2018-04-29 08:56:22 +0300

    ProductContract class

---
+++ app/src/main/java/com/example/android/inventoryapp/database/ProductContract.java
@@ -0,0 +1,23 @@
+package com.example.android.inventoryapp.database;
+
+import android.provider.BaseColumns;
+
+public final class ProductContract {
+
+    // Private constructor
+    private ProductContract() {
+    }
+
+    // Inner class for table
+    public final class Product implements BaseColumns {
+
+        public final static String TABLE_NAME = "products";
+
+        public final static String COLUMN_ID = "_id";
+        public final static String COLUMN_PRODUCT_NAME = "Product name";
+        public final static String COLUMN_PRICE = "Price";
+        public final static String COLUMN_QUANTITY = "Quantity";
+        public final static String COLUMN_SUPPLIER_NAME = "Supplier name";
+        public final static String COLUMN_SUPPLIER_PHONE_NUMBER = "Supplier phone number";
+    }
+}
